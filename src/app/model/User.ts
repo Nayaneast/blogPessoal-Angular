47bb3080bc436dd959e130457ca01d7376b6f882
Nayane Amorim <nayaneast@gmail.com>
2021-04-30 17:05:05 -0300

    consumo de API

---
+++ src/app/model/User.ts
@@ -0,0 +1,12 @@
+import { Postagem } from "./Postagem"
+
+export class User{
+
+    public id: number
+    public nome: String
+    public usuario: String
+    public senha: String
+    public foto: String
+    public tipo: String
+    public postagem:Postagem[]
+}
---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin{
+
+    public id: number
+    public nome: String
+    public usuario: String
+    public senha: String
+    public token: String
+    public foto: String
+    public tipo: String
+}
